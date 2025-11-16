f77718f16474029ca1f0e08829af4a8f06757b94
efrat.tawil <efrattawil@gmail.com>
2025-11-16 11:30:17 +0200

    changes in package.json2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, 
-  },
-  preview: {
-    allowedHosts: [
-      'bank-project-full-2.onrender.com', 
-      'localhost', 
-      '127.0.0.1',
-      'bank-project-full-1.onrender.com', 
-    ],
-  },
 })
